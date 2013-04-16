f1bf002e3c0d85e7510650b5a9bc79f796a91ecb
José Devezas <joseluisdevezas@gmail.com>
2013-04-16 18:11:53 +0100

    Created an adoption score based on rank and reviewed the code.

--- music-listening-behavior/lastfm-data-analysis.R
+++ music-listening-behavior/lastfm-data-analysis.R
@@ -1,28 +1,50 @@
 users = read.table(gzfile("~/Desktop/lastfm-dataset-1K/userid-profile.tsv.gz"), quote="", sep="\t")
 names(users) = c("id", "gender", "age", "country", "registered")
 
-# Data for user 1 only.
+# Data for user 1 only (users were cut into separate files using split-by-user.py).
 songs = read.table("~/Desktop/lastfm-dataset-1K/user_000003.csv", quote="", sep="\t")
 names(songs) = c("id", "timestamp", "artid", "artname", "traid", "traname")
-songs$month.year = strftime(as.Date(songs$timestamp), "%Y-%m")
+songs$month = strftime(as.Date(songs$timestamp), "%Y-%m")
 
-# Top artist each month.
-monthly.top = aggregate(songs$artname, by=list(songs$month.year), function(artname) {
+# Monthly number of plays for each artist.
+monthly.artist.plays = with(songs, aggregate(artname, by=list(month, artname), length))
+names(monthly.artist.plays) = c("month", "artist", "plays")
+
+# Monthly favorite artists.
+monthly.favorite = with(monthly.artist.plays, aggregate(plays, by=list(month), function(monthly.plays) artist[which.max(monthly.plays)]))
+monthly.favorite$artist = as.character(monthly.favorite$artist)
+names(monthly.favorite) = c("month", "artist")
+
+# Monthly artist ranks.
+monthly.ranks = aggregate(songs$artname, by=list(songs$month), function(artname) {
   plays = table(artname)
   names(sort(plays, decreasing=TRUE))
 })
-names(monthly.top) = c("year.month", "top.artists")
+names(monthly.ranks) = c("month", "ranks")
 
-# How many times do number one monthly artists appear ahead in time in the top 5?
-result = list()
-for (i in 1:(nrow(monthly.top$top.artists)-1)) {
-  result[[monthly.top$top.artists[i,1]]] = 0
-  for (j in 1:(nrow(monthly.top$top.artists)-1)) {
-    if (i < j & monthly.top$year.month[i] < monthly.top$year.month[j]) {
-      if (monthly.top$top.artists[i,1] %in% monthly.top$top.artists[j,]) {
-        result[[monthly.top$top.artists[i,1]]] = result[[monthly.top$top.artists[i,1]]] + 1 * 1/j
+# How well do favorite monthly artists rank ahead in time?
+# (This score measures adpotion by considering not only if an artist is listened to again
+# in the future, but also how much it's listened to compared to other artists.)
+adoption.by.rank = list()
+for (i in 1:(nrow(monthly.ranks$ranks)-1)) {
+  adoption.by.rank[[monthly.ranks$ranks[i,1]]] = 0
+  for (j in 1:(nrow(monthly.ranks$ranks)-1)) {
+    if (i < j & monthly.ranks$month[i] < monthly.ranks$month[j]) {
+      if (monthly.ranks$ranks[i,1] %in% monthly.ranks$ranks[j,]) {
+        adoption.by.rank[[monthly.ranks$ranks[i,1]]] = adoption.by.rank[[monthly.ranks$ranks[i,1]]] + 1 * 1/j
       }
     }
   }
 }
-result = sort(unlist(result)/max(unlist(result)), decreasing=TRUE)
+adoption.by.rank = sort(unlist(adoption.by.rank)/max(unlist(adoption.by.rank)), decreasing=TRUE)
+
+# How consistently are favorite monthly artists listened to ahead in time?
+# (Measure variance ahead of time?)
+# result = list()
+# for (i in 1:(nrow(monthly.favorite)-1)) {
+#   result[[monthly.favorite[i,]$artist]] = mean(result[[monthly.favorite[i,]$artist]],
+#     var(monthly.artist.plays[which(
+#       monthly.artist.plays$month > monthly.favorite[i,]$month
+#       & monthly.artist.plays$artist == monthly.favorite[i,]$artist),]$plays), na.rm=TRUE)
+# }
+# result = sort(unlist(result), decreasing=TRUE)
