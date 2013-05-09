679473e082dc08c5372c7b7bd7dbd5cb385c66f2
José Devezas <joseluisdevezas@gmail.com>
2013-05-09 11:13:14 +0100

    Cleaned up the script a little bit and saved computed data for all users in memory, since it's not that much data.

--- music-listening-behavior/lastfm-data-analysis.R
+++ music-listening-behavior/lastfm-data-analysis.R
@@ -1,39 +1,58 @@
-baseDir = "~/Desktop/lastfm-dataset-1K"
+#
+# Configurations and constants.
+#
 
-is.any.na = function(vals) {
+baseDir <- "~/Desktop/lastfm-dataset-1K"
+
+# Dates range from 2005-02-14 00:00:07 to 2013-09-29 18:32:04.
+maxMonthRange <- c("2005-02", "2013-09")
+
+
+#
+# Utility functions.
+#
+
+IsAnyNA <- function(vals) {
   for(val in vals)
     if (is.na(val))
       return(TRUE)
   return(FALSE)
 }
 
-f.expand.artists.months.with.zeros = function(d, month.range=NA) {
-  if (is.any.na(month.range))
-    month.range = range(d$month)
+AddZeroMonths <- function(d, month.range=NA) {
+  if (IsAnyNA(month.range))
+    month.range <- range(d$month)
     
-  all.months = as.character(seq(as.Date(paste(month.range[1], "01", sep="-")),
+  all.months <- as.character(seq(as.Date(paste(month.range[1], "01", sep="-")),
                                 as.Date(paste(month.range[2], "01", sep="-")),
                                 by="1 month"))
-  all.months = unlist(lapply(all.months, function(month) { d = strsplit(month, "-")[[1]]; paste(d[1], d[2], sep="-") }))
-  all.artists = unique(d$artist)
+  all.months <- unlist(lapply(all.months, function(month) {
+    d <- strsplit(month, "-")[[1]]
+    paste(d[1], d[2], sep="-")
+  }))
+  all.artists <- unique(d$artist)
   
-  result = data.frame(month = c(sapply(all.months, function(m) rep(m, length(all.artists)))))
-  result = cbind(result, artist = rep(all.artists, length(all.months)))
-  result = merge(d, result, by.x=c("month", "artist"), by.y=c("month", "artist"), all.y=TRUE)
-  na.pos = which(is.na(result$plays))
+  result <- data.frame(month = c(sapply(all.months, function(m) rep(m, length(all.artists)))))
+  result <- cbind(result, artist = rep(all.artists, length(all.months)))
+  result <- merge(d, result, by.x=c("month", "artist"), by.y=c("month", "artist"), all.y=TRUE)
+  na.pos <- which(is.na(result$plays))
   if (length(na.pos) > 0)
-    result[na.pos,]$plays = 0  
+    result[na.pos,]$plays <- 0  
   
   return(result)
 }
 
-# Dates range from 2005-02-14 00:00:07 to 2013-09-29 18:32:04.
-max.month.range = c("2005-02", "2013-09")
 
-users = read.table(gzfile(paste(baseDir, "userid-profile.tsv.gz", sep="/"), quote="", sep="\t")
-names(users) = c("id", "gender", "age", "country", "registered")
+#
+# Dataset analysis.
+#
 
-user.stats = data.frame(id=levels(users$id),
+users <- read.table(gzfile(paste(baseDir, "userid-profile.tsv.gz", sep="/")), quote="", sep="\t")
+names(users) <- c("id", "gender", "age", "country", "registered")
+
+#-------
+
+user.stats <- data.frame(id=levels(users$id),
                         unique.artists=rep(NA, nrow(users)),
                         plays.total=rep(NA, nrow(users)),
                         plays.min=rep(NA, nrow(users)),
@@ -42,75 +61,84 @@ user.stats = data.frame(id=levels(users$id),
                         plays.mean=rep(NA, nrow(users)),
                         plays.qua3=rep(NA, nrow(users)),
                         plays.max=rep(NA, nrow(users)))
-                   
-for (userId in users$id) {
-  print(paste("Processing ", userId, "...", sep=""))
+
+monthly.artist.plays <- list()
+monthly.ranks <- list()
+adoption.by.rank <- list()
+
+for (user.id in users$id) {
+  print(paste("Processing ", user.id, "...", sep=""))
   
   # Songs listened by the user.
-  songs = read.table(paste(paste(baseDir, userId, sep="/"), "csv", sep="."), quote="", sep="\t", fill=T)
-  names(songs) = c("id", "timestamp", "artid", "artname", "traid", "traname")
-  songs$month = strftime(as.Date(songs$timestamp), "%Y-%m")
+  songs <- read.table(paste(paste(baseDir, user.id, sep="/"), "csv", sep="."), quote="", sep="\t", fill=T)
+  names(songs) <- c("id", "timestamp", "artid", "artname", "traid", "traname")
+  songs$month <- strftime(as.Date(songs$timestamp), "%Y-%m")
   
   # Monthly number of plays for each artist.
-  monthly.artist.plays = with(songs, aggregate(artname, by=list(month, artname), length))
-  names(monthly.artist.plays) = c("month", "artist", "plays")
+  monthly.artist.plays[[user.id]] <- with(songs, aggregate(artname, by=list(month, artname), length))
+  names(monthly.artist.plays[[user.id]]) <- c("month", "artist", "plays")
   
   # Unique artists listened by the user.
-  user.stats[which(user.stats$id == userId),]$unique.artists = nlevels(monthly.artist.plays$artist)
+  user.stats[which(user.stats$id == user.id),]$unique.artists <- nlevels(monthly.artist.plays[[user.id]]$artist)
   
   # Statistics for the play count of the user.
-  user.stats[which(user.stats$id == userId),]$plays.total = sum(monthly.artist.plays$plays)
-  user.stats[which(user.stats$id == userId), 4:9] = summary(monthly.artist.plays$plays)
+  user.stats[which(user.stats$id == user.id),]$plays.total <- sum(monthly.artist.plays[[user.id]]$plays)
+  user.stats[which(user.stats$id == user.id), 4:9] <- summary(monthly.artist.plays[[user.id]]$plays)
         
-  # Top overall artists.
-  overall.ranks = with(monthly.artist.plays, aggregate(artist, by=list(artist), length))
-  names(overall.ranks) = c("artist", "plays")
-  overall.ranks = overall.ranks[order(overall.ranks$plays, decreasing=T),]
-  
-  # Fill the monthly time series with zeros for missing values and export to CSV.
-  write.csv(f.expand.artists.months.with.zeros(monthly.artist.plays[
-    which(monthly.artist.plays$artist %in% overall.ranks[1:20, ]$artist),]),
-            file=paste(paste(baseDir, userId, sep="/"), "_monthly_activity.csv", sep=""),
-            row.names=FALSE)
-  
-  # Monthly favorite artists.
-  monthly.favorite = with(monthly.artist.plays, aggregate(plays, by=list(month), function(monthly.plays) artist[which.max(monthly.plays)]))
-  names(monthly.favorite) = c("month", "artist")
-  monthly.favorite$artist = as.character(monthly.favorite$artist)
-
   # Monthly artist ranks.
-  monthly.ranks = aggregate(songs$artname, by=list(songs$month), function(artname) {
-    plays = table(artname)
+  monthly.ranks[[user.id]] <- aggregate(songs$artname, by=list(songs$month), function(artname) {
+    plays <- table(artname)
     names(sort(plays, decreasing=TRUE))
   })
-  names(monthly.ranks) = c("month", "ranks")
-  
+  names(monthly.ranks[[user.id]]) <- c("month", "ranks")
+    
   # How well do favorite monthly artists rank ahead in time?
   # (This score measures adpotion by considering not only if an artist is listened to again
   # in the future, but also how much it's listened to compared to other artists.)
-  adoption.by.rank = list()
-  for (i in 1:(nrow(monthly.ranks$ranks))) {
-    adoption.by.rank[[monthly.ranks$ranks[i,1]]] = 0
-    for (j in 1:(nrow(monthly.ranks$ranks))) {
-      if (i < j & monthly.ranks$month[i] < monthly.ranks$month[j]) {
-        if (monthly.ranks$ranks[i,1] %in% monthly.ranks$ranks[j,]) {
-          adoption.by.rank[[monthly.ranks$ranks[i,1]]] = adoption.by.rank[[monthly.ranks$ranks[i,1]]] + 1 * 1/j
+  user.adoption.by.rank <- list()
+  for (i in 1:(nrow(monthly.ranks[[user.id]]$ranks))) {
+    user.adoption.by.rank[[monthly.ranks[[user.id]]$ranks[i, 1]]] <- 0
+    for (j in 1:(nrow(monthly.ranks[[user.id]]$ranks))) {
+      if (i < j & monthly.ranks[[user.id]]$month[i] < monthly.ranks[[user.id]]$month[j]) {
+        if (monthly.ranks[[user.id]]$ranks[i, 1] %in% monthly.ranks[[user.id]]$ranks[j,]) {
+          user.adoption.by.rank[[monthly.ranks[[user.id]]$ranks[i, 1]]] <-
+            user.adoption.by.rank[[monthly.ranks[[user.id]]$ranks[i, 1]]] + 1 * 1/j
         }
       }
     }
   }
-  adoption.by.rank = sort(unlist(adoption.by.rank)/max(unlist(adoption.by.rank)), decreasing=TRUE)
+  user.adoption.by.rank <- sort(unlist(user.adoption.by.rank)/max(unlist(user.adoption.by.rank)), decreasing=TRUE)
+  adoption.by.rank[[user.id]] <- user.adoption.by.rank
   
   # How consistently are favorite monthly artists listened to ahead in time?
   # (Measure variance ahead of time?)
-  # result = list()
-  # for (i in 1:(nrow(monthly.favorite)-1)) {
-  #   result[[monthly.favorite[i,]$artist]] = mean(result[[monthly.favorite[i,]$artist]],
-  #     var(monthly.artist.plays[which(
-  #       monthly.artist.plays$month > monthly.favorite[i,]$month
-  #       & monthly.artist.plays$artist == monthly.favorite[i,]$artist),]$plays), na.rm=TRUE)
-  # }
-  # result = sort(unlist(result), decreasing=TRUE)
 }
+
+
+#
+# Write results to files.
+#
                    
-write.csv(user.stats, file=paste(baseDir, "user-stats.csv", sep="/"), row.names=FALSE)
+write.csv(user.stats, file=paste(baseDir, "user-stats.csv", sep="/"), row.names=FALSE)                   
+
+for (user.id in users$id) {
+  print(paste("Writing analysis data for ", user.id, "...", sep=" "))
+  
+  # Top overall artists.
+  overall.ranks <- with(monthly.artist.plays[[user.id]], aggregate(artist, by=list(artist), length))
+  names(overall.ranks) <- c("artist", "plays")
+  overall.ranks <- overall.ranks[order(overall.ranks$plays, decreasing=T),]
+  
+  # Fill the monthly time series with zeros for missing values and export to CSV.
+  write.csv(AddZeroMonths(monthly.artist.plays[[user.id]][
+    which(monthly.artist.plays[[user.id]]$artist %in% overall.ranks[1:20, ]$artist),]),
+          file=paste(paste(baseDir, user.id, sep="/"), "_monthly_activity.csv", sep=""),
+          row.names=FALSE)
+}
+
+
+#
+# Clean up temporary variables.
+#
+
+rm(i, j, user.id, plays, month, artists, overall.ranks, songs)
