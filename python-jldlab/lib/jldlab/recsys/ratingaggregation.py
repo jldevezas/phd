Solved a critical bug with the normalization method and the group rating aggregation by weighted usage mean.

python-jldlab/lib/jldlab/recsys/ratingaggregation.py
@@ -7,7 +7,7 @@
 
 def group_weighted_mean_by_user_activity(group_vectors):
 	weights = np.array([np.count_nonzero(group_vector) for group_vector in group_vectors])
-	weights = normalize(1 - weights / float(np.max(weights)), scale=(0.3, 0.7))
+	weights = normalize(1 - weights / float(np.max(weights)), scale=(0.3, 0.7), keep_zeros=False)
 	return np.sum([weight * np.array(group_vector)
 		for group_vector, weight in zip(group_vectors, weights)], axis=0)
 

python-jldlab/lib/jldlab/utils.py
@@ -7,7 +7,7 @@
 
 import numpy as np
 
-def normalize(vector, scale=(0, 1)):
+def normalize(vector, scale=(0, 1), keep_zeros=True):
 	if len(vector) == 0:
 		return vector
 	min_vector = min(vector)
@@ -16,8 +16,9 @@ def normalize(vector, scale=(0, 1)):
 		return list(np.zeros(len(vector)))
 	result = []
 	for i in xrange(len(vector)):
-		if vector[i] == 0:
+		if vector[i] == 0 and keep_zeros:
 			result.append(0) # TODO this should come from behind as None if it works
 		else:
-			result.append(((scale[1] - scale[0]) * (vector[i] - min_vector)) / float(max_vector - min_vector))
+			result.append(((scale[1] - scale[0]) * (vector[i] - min_vector))
+					/ float(max_vector - min_vector) + scale[0])
 	return np.array(result)