746cbd8a3e84e7bcc6efc44bf8e09b435fa8898a
José Devezas <joseluisdevezas@gmail.com>
2013-10-23 11:14:16 +0100

    Updated script to distinguish between the evaluation of the first and second iterations of the system.

--- usability/r-workspace/sus.R
+++ usability/r-workspace/sus.R
@@ -1,8 +1,18 @@
+analyze <- function(sus.q) {
+  sus.q[, seq(1, length(sus.q), by=2)] <- sus.q[, seq(1, length(sus.q), by=2)] - 1
+  sus.q[, seq(2, length(sus.q), by=2)] <- 5 - sus.q[, seq(2, length(sus.q), by=2)]
+  
+  sus <- apply(sus.q, 1, sum) * 2.5
+  print(summary(sus))
+  hist(sus, xlim=c(0,100))
+}
+
 eval <- read.csv("~/Desktop/juggle-eval.csv")
 
-sus.q <- eval[, 6:15]
-sus.q[, seq(1, length(sus.q), by=2)] <- sus.q[, seq(1, length(sus.q), by=2)] - 1
-sus.q[, seq(2, length(sus.q), by=2)] <- 5 - sus.q[, seq(2, length(sus.q), by=2)]
+first.iteration <- 1:7
+second.iteration <- 8:nrow(eval)
 
-sus <- apply(sus.q, 1, sum) * 2.5
-summary(sus)
+sus.q <- eval[, 6:15]
+
+analyze(sus.q[first.iteration, ])
+analyze(sus.q[second.iteration, ])
