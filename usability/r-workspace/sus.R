d77a3bf55ea59ede71bb1bee0fc07b5520e2a8c4
José Devezas <joseluisdevezas@gmail.com>
2013-10-31 14:15:09 +0000

    Created some charts to better look at the survey scores.

--- usability/r-workspace/sus.R
+++ usability/r-workspace/sus.R
@@ -1,10 +1,56 @@
-analyze <- function(sus.q) {
+analyze.usability <- function(sus.q, plots=TRUE) {
+#   require(ggplot2)
+  
   sus.q[, seq(1, length(sus.q), by=2)] <- sus.q[, seq(1, length(sus.q), by=2)] - 1
   sus.q[, seq(2, length(sus.q), by=2)] <- 5 - sus.q[, seq(2, length(sus.q), by=2)]
   
   sus <- apply(sus.q, 1, sum) * 2.5
-  print(summary(sus))
-  hist(sus, xlim=c(0,100))
+    
+  if (plots) {
+    hist(sus, xlim=c(0,100), xlab="SUS Score", ylab="Nummber of Users",
+         main="SUS Score Distribution", col="WhiteSmoke")
+#     print(qplot(sus, geom="histogram", binwidth=10, xlim=c(0,100), xlab="SUS Value", ylab="Number of Users"))
+  }
+  
+  return(list(sus=sus, sus.summary=summary(sus), sus.std=sd(sus)))
+}
+
+analyze.recs <- function(recs, plots=TRUE) {
+#   require(ggplot2)
+  
+  cor.m <- cor(recs)
+  colnames(cor.m) <- NULL
+  rownames(cor.m) <- NULL
+
+  if (plots) {
+    par(mfrow=c(2,2))
+    hist(recs[, 1], xlim=c(0, 10), col="WhiteSmoke",
+         xlab="Quality Score",
+         ylab="Number of Users",
+         main="Quality of Recommendations")
+    hist(recs[, 2], xlim=c(0, 30), col="WhiteSmoke",
+         xlab="Number of Awful Recommendations",
+         ylab="Number of Users",
+         main="Awuful Recommendations")
+    hist(recs[, 3], xlim=c(0, 30), col="WhiteSmoke",
+         xlab="Number of Great Recommendations",
+         ylab="Number of Users",
+         main="Great Recommendations")
+    hist(recs[, 4], xlim=c(0, 30), col="WhiteSmoke",
+         xlab="Number of Captivating New Recommendations",
+         ylab="Number of Users",
+         main="Captivating New Recommendations")
+#     print(qplot(recs[, 1], geom="histogram", binwidth=1, xlim=c(0, 10),
+#                 xlab="Quality of Recommendations", ylab="Number of Users"))
+#     print(qplot(recs[, 2], geom="histogram", binwidth=3, xlim=c(0, 30),
+#                 xlab="Number of Awful Recommendations", ylab="Number of Users"))
+#     print(qplot(recs[, 3], geom="histogram", binwidth=3, xlim=c(0, 30),
+#                 xlab="Number of Great Recommendations", ylab="Number of Users"))
+#     print(qplot(recs[, 4], geom="histogram", binwidth=3, xlim=c(0, 30),
+#                 xlab="Number of Captivating New Recommendations", ylab="Number of Users"))
+  }
+  
+  return(list(recs.summary=summary(recs), recs.std=sapply(recs, sd), cor.matrix=cor.m))
 }
 
 eval <- read.csv("~/Desktop/juggle-eval.csv")
@@ -14,5 +60,9 @@ second.iteration <- 8:nrow(eval)
 
 sus.q <- eval[, 6:15]
 
-analyze(sus.q[first.iteration, ])
-analyze(sus.q[second.iteration, ])
+par(mfrow=c(1,2))
+analyze.usability(sus.q[first.iteration, ])
+analyze.usability(sus.q[second.iteration, ])
+
+recs <- eval[, 16:(ncol(eval)-1)]
+analyze.recs(recs)
